cfaf378b376b9c9ad317eeff4652b67b366a3217
merry-ali <merry.m.ali@upeu.edu.pe>
2025-04-24 20:57:25 -0500

    examen1

---
+++ EXAMEN/practica8.java
@@ -0,0 +1,21 @@
+package EXAMEN;
+
+import javax.swing.JOptionPane;
+
+public class practica8 {
+    public static void main(String[] args) {
+        int n=Integer.parseInt(JOptionPane.showInputDialog("ingrese el numero 1 :"));
+        int mayor = n;
+        int menor = n;
+        for (int i = 2; i <= 10; i++) {
+            n=Integer.parseInt(JOptionPane.showInputDialog("ingrese el numero "+i+" :"));
+           
+            if (n>mayor) mayor = n;
+            if (n<menor) menor = n;
+        }
+//ñ{.-,´+}¿'.^~\\
+        JOptionPane.showMessageDialog(null, "el numero mayor es :" + mayor+" \nel numero menor es :" + menor);
+        
+    }
+    
+}
